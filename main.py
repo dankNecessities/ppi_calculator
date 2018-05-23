1d8c7181a20f727ca53b0d44ca5bfb7b72e4b318
DankNeccesities <sam.olwe@gmail.com>
2018-05-24 01:00:15 +0300

    Added First questionnaire page

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class navBtn(QPushButton):
 	def __init__(self, parent=None):
 		super(navBtn, self).__init__(parent)
 		self.setFlat(True)
+		self.setMinimumSize(90, 30)
 		self.setMaximumSize(90, 30)
 		self.setStyleSheet('''
 			border-width: 2px 2px 2px 2px;
@@ -46,20 +47,32 @@ class navBtn(QPushButton):
 	def click_close(self):
 		sys.exit()
 
-class Window(QWidget):
-	def __init__(self, parent=None):
-		super(Window, self).__init__(parent)
-		self.setWindowTitle("Hello")
-		self.resize(400, 400)
-		self.setStyleSheet('''
+class UIMain(QWidget):
+	def setupUI(self, Window):
+		Window.resize(400, 400)
+
+		self.QtStack = QStackedLayout()
+		#new widget object for each window
+		self.stack1 = QWidget()
+		self.stack2 = QWidget()
+
+		self.setupHomeUI()
+		self.setupPage1UI()
+
+		self.QtStack.addWidget(self.stack1)
+		self.QtStack.addWidget(self.stack2)
+		
+	def setupHomeUI(self):
+		#self.setWindowTitle("Hello")
+		self.stack1.resize(400, 400)
+		self.stack1.setStyleSheet('''
 				border-style: none;
 				background-color: #101e41;
 			''')
-
 		layout = QVBoxLayout()
 		layout.setContentsMargins(5, 5, 5, 5)
 		layout.setSpacing(0)
-		self.setLayout(layout)
+		self.stack1.setLayout(layout)
 
 		#Heading
 		label1 = QLabel("PPI Calculator")
@@ -101,13 +114,62 @@ class Window(QWidget):
 
 		close_button = navBtn("Close")
 		close_button.clicked.connect(navBtn.click_close)
-		next_button = navBtn("Next")
+		self.page1_button = navBtn("Next")
 		layout.addWidget(gbox)
 		hbox.setSpacing(150)
 		hbox.addWidget(close_button)
-		hbox.addWidget(next_button)
+		hbox.addWidget(self.page1_button)
 
-		self.show()
+	def setupPage1UI(self):
+		self.stack2.resize(400, 400)
+		self.stack2.setStyleSheet('''
+				border-style: none;
+				background-color: #101e41;
+			''')
+		layout = QVBoxLayout()
+		layout.setContentsMargins(5, 5, 5, 5)
+		layout.setSpacing(0)
+		self.stack2.setLayout(layout)
+
+		#Heading
+		label1 = QLabel("Questionnaire")
+		label1.setAlignment(QtCore.Qt.AlignCenter)
+		label1.setStyleSheet('''
+			color: white;
+			font-size: 55px;
+			font-family: "Sawasdee";
+			text-align: center;
+			''')
+		layout.addWidget(label1)
+
+		gbox = QGroupBox("")
+		hbox = QHBoxLayout()
+		gbox.setLayout(hbox)
+
+		self.page0_button = navBtn("Back")
+		close_button = navBtn("Close")
+		close_button.clicked.connect(navBtn.click_close)
+		self.page2_button = navBtn("Next")
+		layout.addWidget(gbox)
+		hbox.setSpacing(50)
+		hbox.addWidget(self.page0_button)
+		hbox.addWidget(close_button)
+		hbox.addWidget(self.page2_button)
+
+class Window(QMainWindow, UIMain):
+	def __init__(self, parent=None):
+		super(Window, self).__init__(parent)
+		self.setupUI(self)
+		self.page1_button.clicked.connect(self.openPage1UI)
+		self.page0_button.clicked.connect(self.openHomeUI)
+
+	def openHomeUI(self):
+		print("Back")
+		self.QtStack.setCurrentIndex(0)
+
+	def openPage1UI(self):
+		print("Next")
+		self.QtStack.setCurrentIndex(1)
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
